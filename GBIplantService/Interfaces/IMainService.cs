d65b69524c3fca753657979fcdb38d009bc25a9b
hubbysmall <av1er2yz8ok9@mail.ru>
2018-06-09 01:05:22 +0400

    lab16

--- GBIplantService/Interfaces/IMainService.cs
+++ GBIplantService/Interfaces/IMainService.cs
@@ -1,4 +1,5 @@
-﻿using GBIplantService.BindingModels;
+﻿using GBIplantService.Attributes;
+using GBIplantService.BindingModels;
 using GBIplantService.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -8,18 +9,20 @@ using System.Threading.Tasks;
 
 namespace GBIplantService.Interfaces
 {
+    [CustomInterface("Интерфейс для работы с заказами")]
     public interface IMainService
     {
+        [CustomMethod("Метод получения списка заказов")]
         List<ZakazViewModel> GetList();
-
+        [CustomMethod("Метод создания заказа")]
         void CreateZakaz(ZakazBindingModel model);
-
+        [CustomMethod("Метод передачи заказа в работу")]
         void TakeZakazInWork(ZakazBindingModel model);
-
+        [CustomMethod("Метод передачи заказа на оплату")]
         void FinishZakaz(int id);
-
+        [CustomMethod("Метод фиксирования оплаты по заказу")]
         void PayZakaz(int id);
-
+        [CustomMethod("Метод пополнения компонент на складе")]
         void PutGBIingridientInStorage(Storage__GBIingridientBindingModel model);
     }
 }
